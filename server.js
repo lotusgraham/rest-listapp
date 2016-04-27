6e82116a05116b32005de590f5e208d3485ea08e
lotusgraham <gwhitley99@gmail.com>
2016-04-27 10:58:49 -0400

    add files and install express and body-parser

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+
+var Storage = function() {
+    this.items = [];
+    this.id = 0;
+};
+
+Storage.prototype.add = function(name) {
+    var item = {
+        name: name,
+         id: this.id
+     };
+    this.items.push(item);
+    this.id += 1;
+    return item;
+};
+
+var storage = new Storage();
+storage.add('A New Car!');
+storage.add('Bluetooth Headset');
+storage.add('Macbook Air');
+
+var app = express();
+app.use(express.static('public'));
+
+app.get('/items', function(req, res) {
+    res.json(storage.items);
+});
+
+
+
+app.listen(process.env.PORT || 8080);
