306b4ed49b33b7aff2d48df2ad78ee1ae2b23a20
OoogleBoogle <simattfield@gmail.com>
2016-04-27 17:34:32 -0700

    Storage constructor is now a module

--- server.js
+++ server.js
@@ -1,42 +1,7 @@
 var express = require('express');
-
-var Storage = function() {
-    this.items = [];
-    this.id = 0;
-};
-
-Storage.prototype.add = function(name) {
-    var item = {
-        name: name,
-        id: this.id
-    };
-    this.items.push(item);
-    this.id += 1;
-    return item;
-};
-
-Storage.prototype.removeitem = function(id) {
-    this.items.forEach(function(item) {
-        if (item.id === parseInt(id, 10)) {
-            var index = storage.items.indexOf(item);
-            storage.items.splice(index, 1);
-        }
-    });
-};
-
-Storage.prototype.editItem = function(requestObj) {
-    this.items.forEach(function(item) {
-        if (item.id === requestObj.id) {
-            item.name = requestObj.name;
-        }
-    });
-
-};
+var Storage = require('./storage.js'); // requiring our Storage constructor
 
 var storage = new Storage();
-storage.add('A New Car!');
-storage.add('Bluetooth Headset');
-storage.add('Macbook Air');
 
 var app = express();
 app.use(express.static('public'));
@@ -51,19 +16,18 @@ app.post('/items', jsonParser, function(req, res) {
     if (!req.body) {
         return res.sendStatus(400);
     }
-
     var item = storage.add(req.body.name);
     res.status(201).json(item);
 });
 
 app.delete('/items/:id', function(req, res) {
     var id = req.params.id;
-    storage.removeitem(id);
+    storage.remove(id);
     res.status(200).json(storage.items);
 });
 
 app.put('/items/:id', jsonParser, function(req, res) {
-    storage.editItem(req.body);
+    storage.edit(req.body);
     res.status(200);
 });
 
---
+++ storage.js
@@ -0,0 +1,33 @@
+module.exports = Storage; // this now means we can export our Storage contsrutor anywhere in our app. 
+function Storage() {
+    this.items = [];
+    this.id = 0;
+};
+
+Storage.prototype.add = function(name) {
+    var item = {
+        name: name,
+        id: this.id
+    };
+    this.items.push(item);
+    this.id += 1;
+    return item;
+};
+
+Storage.prototype.remove = function(id) {
+    var that = this; // storing current context of 'this' (the storage object) as it changes inside the forEach loop to the specific item the loop has stopped on.
+    this.items.forEach(function(item) {
+        if (item.id === parseInt(id, 10)) {
+            var index = that.items.indexOf(item); // 'that' refers to the storage obj as noted above.
+            that.items.splice(index, 1);          // previously we used 'storage' here instead of 'this/that'.
+        }                                         // So the method would have broken if we'd called our storage anything but 'storage'
+    });
+};
+
+Storage.prototype.edit = function(requestObj) {
+    this.items.forEach(function(item) {
+        if (item.id === requestObj.id) {
+            item.name = requestObj.name;
+        }
+    });
+};
